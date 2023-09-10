c3393bc288425c2f49c6997712fa38247eec6a27
rubenSanz18 <140351422+rubenSanz18@users.noreply.github.com>
2023-09-10 19:14:37 +0200

    Delete Routes/country.js

--- Routes/country.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const countryController = require("../Controllers/country");
-const auth = require("../Middlewares/auth");
-
-router.post("/uploadContinents", auth.auth, countryController.uploadCountries);
-
-module.exports = router;
