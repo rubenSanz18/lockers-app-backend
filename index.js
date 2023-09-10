743c0e19e6d90492c289a95e7a8730f08b4a78e4
Rubén Sanz Barrio <ruben.sanz@alumnos.upm.es>
2023-09-10 19:43:14 +0200

    index.js after deleting country, city and continent models

--- index.js
+++ index.js
@@ -21,16 +21,10 @@ app.use(express.urlencoded({extended: true}));
 const userRoutes = require("./Routes/user");
 const packetRoutes = require("./Routes/packet");
 const lockerRoutes = require("./Routes/locker");
-const countryRoutes = require("./Routes/country");
-const cityRoutes = require("./Routes/city");
-const continentRoutes = require("./Routes/continent");
 
 app.use("/api/user", userRoutes);
 app.use("/api/packet", packetRoutes);
 app.use("/api/locker", lockerRoutes);
-app.use("/api/continent", continentRoutes);
-app.use("/api/country", countryRoutes);
-app.use("/api/city", cityRoutes);
 
 //Server listens to HTTP Requests
 app.listen(port, () => {
