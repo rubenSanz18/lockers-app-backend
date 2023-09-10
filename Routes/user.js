44bdc05ea280091da8b354902eadac825938d375
Rubén Sanz Barrio <ruben.sanz@alumnos.upm.es>
2023-09-10 21:02:50 +0200

    Error at delete user route fixed

--- Routes/user.js
+++ Routes/user.js
@@ -7,6 +7,6 @@ router.post("/register", userController.register);
 router.put("/login", userController.login);
 router.put("/update", auth.auth, userController.update);
 router.get("/get", auth.auth, userController.getUser);
-router.delete("delete", auth.auth, userController.deleteUser);
+router.delete("/delete", auth.auth, userController.deleteUser);
 
 module.exports = router;
